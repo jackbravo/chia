e05b2385cfc95c32b7e4ecda714f502a63bf5203
Joaquin Bravo Contreras <jackbravo@gmail.com>
2010-07-23 13:46:47 -0500

    Started chia project

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/usr/share/php5/symfony-1.4/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
